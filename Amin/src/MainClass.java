305b732d2ab12deeb4828c9e6500a5a42465c916
Amin Company <amin.com8@outlook.com>
2020-03-27 04:45:43 +0430

    this is second commit

---
+++ Amin/src/MainClass.java
@@ -0,0 +1,11 @@
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("welcome");
+		System.out.println("this is second");
+
+	}
+
+}
